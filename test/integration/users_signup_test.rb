de532022437ed7f22e9a09833459903d2e643306
NathanielBampo <mergontv@gmail.com>
2022-10-18 10:25:19 +0400

    Home_7.7 added Test user valid registration and erorr in message

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -25,5 +25,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/show'
+
+    assert_not flash.nil?
+  end
+
+  test 'messages in errors' do
+    get signup_path
+
+    assert_no_difference 'User.count' do
+      post users_path, user: { name: '',
+                               email: 'user@invalid',
+                               password: '12345',
+                               password_confirmation: '54321' }
+    end
+    assert_template 'users/new'
+
+    assert_select 'div#<CSS id for error_explanation>'
+    assert_select 'div.<CSS class for field with error>'
+
+    assert_select 'li', text: "Name can't be blank"
+    assert_select 'li', text: 'Email is invalid'
+    assert_select 'li', text: "Password confirmation doesn't match Password"
+    assert_select 'li', text: 'Password is too long (maximum is 2 characters)'
   end
 end
