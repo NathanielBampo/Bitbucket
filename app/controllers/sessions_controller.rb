4b76a2cda32450250a892f4cb5b3ef782ba0d190
NathanielBampo <mergontv@gmail.com>
2022-11-16 14:08:45 +0400

    add Session for user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email.downcase]) # Find equal email
+
+    
+    if user && user.authenticate(params[:session][:password]) # test equal password
+      session[:user_id] = user.id
+
+      # User input and render profile user
+      flash[:success] = "You log in"
+      redirect_to user_path(user)
+    else
+      flash[:danger] = 'Invalide email or password combination'
+      render "new" # render form_field new session and print error
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+
+    flash[:danger] = "You log out account"
+    redirect_to root_path
+  end
+end
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionController::TestCase
+  test "should get new" do
+
+  end
+
+end
